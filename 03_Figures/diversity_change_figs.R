d8b1eac5c171b41a2a38408a718509fecd0ba39d
Leah Veldhuisen <leah.veldhuisen@gmail.com>
2025-11-04 14:57:58 -0700

    Update diversity_change_figs.R

--- 03_Figures/diversity_change_figs.R
+++ 03_Figures/diversity_change_figs.R
@@ -105,9 +105,13 @@ all_pred_fig_NL <- ggplot(all_pred_NL) +
                   linewidth = 1.3)+
   theme_classic(base_size = 22)+
   ylab(Delta ~ "SES")+
-  xlab("Community type")+
+  xlab("Community Type")+
   scale_color_manual(values=c("#8FD744FF","#287C8EFF","#440154FF"))+
-  geom_hline(yintercept = 0, color = "gray", linetype = "dashed", linewidth = 1.3)
+  geom_hline(yintercept = 0, color = "gray", linetype = "dashed", linewidth = 1.3)+
+  theme(
+    legend.position = c(0.9, 0.9),
+    legend.background = element_rect(fill = alpha("white", 0.6), color = "black"))
+
 
 plot(all_pred_fig_NL)
 ggsave("Figures/LM_NOlumped.png", width = 12, height = 7, dpi = 600)
